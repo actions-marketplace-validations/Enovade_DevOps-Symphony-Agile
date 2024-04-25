b4f8e7ecd88cbe40220b5da81e5ff90f160d8aab
Hanafiah <hanafiah@cloud-connect.asia>
2024-04-25 08:02:38 +0800

    add AI process

--- dist/index.js
+++ dist/index.js
@@ -37796,50 +37796,31 @@ const { owner, repo } = context.repo;
 console.log(octokit.GITHUB_TOKEN)
 console.log(owner, repo)
 
-const docPath = core.getInput('path', { required: true });
-const encoding = core.getInput('encoding', { required: true });
+const agileURL = core.getInput('agileURL', { required: true });
 
-console.log(docPath)
-console.log(encoding)
+const mymd = core.getInput('result', { required: true });
 
-var analyzeData = {}
-var recommendData = {}
+console.log(mymd)
+
+var agileData = {}
 var resultData = ""
 
-// let payload = {
-//     "question" : jsondata
-// }
-// let getAnalyze = axios.post(analyzeURL, payload)
-// .then(res => {
-//     if (res.status === 200) {
-//         const analyzeData = res.data.text
-//         console.log("-------------------------------------------------")
-//         console.log(analyzeData)
-//         console.log("-------------------------------------------------\n")
-//         const analyzePayload = {
-//             "question" : analyzeData
-//         }
-//         let getRecommend = axios.post(recommendURL, analyzePayload)
-//         .then(resRecommend => {
-//             if (res.status === 200) {
-//                 //console.log(resRecommend.data)
-//                 //let myData = JSON.stringify(res.data)
-//                 const recommendData = resRecommend.data.text
-//                 console.log("-------------------------------------------------\n")
-//                 console.log(recommendData)
-//                 console.log("-------------------------------------------------\n")
-//                 resultData = "# K6.io Performance & Stress Test Report\n" + "\n## Reports\n"+ analyzeData + "\n## Recommendation\n" + recommendData + "\n";
-//                 console.log("-------------------------------------------------\n")
-//                 console.log(resultData)
-
-//                 core.setOutput('jawapan', resultData);
-//             }
-//         })
-//     }
-// })
-// .catch(err => {
-//     console.error(err); 
-// })
+ let payload = {
+     "question" : mymd
+ }
+ let getAnalyze = axios.post(agileURL, payload)
+.then(res => {
+    if (res.status === 200) {
+        const agileData = res.data.text
+        console.log("-------------------------------------------------")
+        console.log(agileData)
+        console.log("-------------------------------------------------\n")
+        core.setOutput('jawapan', "Successfully create tasks");
+    }
+})
+.catch(err => {
+    console.error(err); 
+})
 })();
 
 module.exports = __webpack_exports__;
